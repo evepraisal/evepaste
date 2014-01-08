ea2afdb3a99626da5cabf165793b239740f13ca5
Kevin McDonald <k3vinmcdonald@gmail.com>
2014-01-07 18:26:23 -0600

    Removes un-needed survey scanner ignore regex

--- evepaste/parsers/survey_scanner.py
+++ evepaste/parsers/survey_scanner.py
@@ -12,7 +12,6 @@ SURVEY_SCANNER_RE = re.compile(r"""^([\S ]+)\t           # name
                                     ([\d ,\.]+)\t        # quantity
                                     ([\d ,\.]* (m|km))$  # distance
                                 """, re.X)
-SURVEY_SCANNER_IGNORE_RE = re.compile(r"^([\S ]+)")
 
 
 def parse_survey_scanner(lines):
@@ -21,7 +20,6 @@ def parse_survey_scanner(lines):
     :param string paste_string: A survey scanner result string
     """
     matches, bad_lines = regex_match_lines(SURVEY_SCANNER_RE, lines)
-    _, bad_lines2 = regex_match_lines(SURVEY_SCANNER_IGNORE_RE, bad_lines)
 
     result = [{'name': name,
                'quantity': f_int(quantity),
