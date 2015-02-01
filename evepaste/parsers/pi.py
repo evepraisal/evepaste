50597b3d138c82838de4bc9e1395f21ed9fef8e3
Kevin McDonald <k3vinmcdonald@gmail.com>
2015-02-01 17:52:12 -0600

    Fixes PI parsing

--- evepaste/parsers/pi.py
+++ evepaste/parsers/pi.py
@@ -6,16 +6,16 @@ Parse planetary interaction results.
 """
 import re
 
-from evepaste.utils import regex_match_lines, f_int
+from evepaste.utils import regex_match_lines
 
 PI_RE = re.compile(r"""^([\d,'\.]+)\t            # quantity
                         ([\S ]+)\t               # name
                         ((Routed|Not\ routed))$  # routed
                     """, re.X)
 
-PI_RE2 = re.compile(r"""^\t            # icon
-                         ([\S ]+)\t    # name
-                         ([\d,'\.]+)\t # quantity
+PI_RE2 = re.compile(r"""^\t             # icon
+                         ([\S ]+)\t     # name
+                         ([\d,'\.]+)\t  # quantity
                          ([\d,'\.]+)$   # volume
                      """, re.X)
 
@@ -35,15 +35,15 @@ def parse_pi(lines):
     matches3, bad_lines3 = regex_match_lines(PI_RE3, bad_lines2)
 
     result = [{'name': name,
-               'quantity': f_int(quantity),
+               'quantity': float(quantity),
                'routed': routed == 'Routed'}
               for quantity, name, routed, _ in matches]
     result2 = [{'name': name,
-                'quantity': f_int(quantity),
-                'volume': volume}
+                'quantity': float(quantity),
+                'volume': float(volume)}
                for name, quantity, volume in matches2]
     result3 = [{'name': name,
-                'quantity': f_int(quantity)}
+                'quantity': float(quantity)}
                for name, quantity in matches3]
 
     return result + result2 + result3, bad_lines3
--- tests/parsers/pi.py
+++ tests/parsers/pi.py
@@ -14,13 +14,13 @@ PI_TABLE.add_test('''331.0\tAqueous Liquids\tNot routed
 331\tAqueous Liquids\tRouted''', ([
                   {'routed': False,
                    'name': 'Aqueous Liquids',
-                   'quantity': 3310},
+                   'quantity': 331},
                   {'routed': True,
                    'name': 'Aqueous Liquids',
                    'quantity': 331}], []))
 PI_TABLE.add_test('''\tAqueous Liquids\t305.0\t3.05''', ([
-                  {'volume': '3.05',
+                  {'volume': 3.05,
                    'name': 'Aqueous Liquids',
-                   'quantity': 3050}], []))
+                   'quantity': 305.0}], []))
 PI_TABLE.add_test('''\tAqueous Liquids\t205.0''', ([
-                  {'name': 'Aqueous Liquids', 'quantity': 2050}], []))
+                  {'name': 'Aqueous Liquids', 'quantity': 205}], []))
